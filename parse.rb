7e132be8cb54369d1c9a49b1c7359489d98c343a
Simon Russell <simon@bellyphant.com>
2010-01-25 23:08:13 +1100

    show C code from parsed stuff

--- parse.rb
+++ parse.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 require 'lib/ruby_native'
 
-pp_sexp STDOUT, RubyParser.new.parse(ARGV.join(' '))
+parsed = RubyParser.new.parse(ARGV.join(' '))
+pp_sexp STDOUT, parsed
+puts "-------------------------"
+puts RubyNative::ExpressionCompiler.new.compile(parsed)
