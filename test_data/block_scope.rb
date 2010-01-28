4c2f55ef3eef720fce288ccd3db9bdd00693b4f1
Simon Russell <simon@bellyphant.com>
2010-01-28 16:18:38 +1100

    block scope test

---
+++ test_data/block_scope.rb
@@ -0,0 +1,38 @@
+def test1
+  x = 1
+
+  [1,2,3].each do |x|
+  end
+
+  x == 3
+end
+
+def test2
+  x = 1
+
+  [1,2,3].each do |y|
+    x = y
+  end
+
+  x == 3
+end
+
+def test3
+  [1,2,3].each do |y|
+    x = y
+  end
+
+  !defined?(x)
+end
+
+def test4
+  [1,2,3].each do |y|
+    x = y
+    return !!defined?(x)
+  end
+end
+
+puts test1
+puts test2
+puts test3
+puts test4
